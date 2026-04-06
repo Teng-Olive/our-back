b86bca38e3d4b4948e13744bd002cb6234d2b7e7
Teng-Olive <119424685+Teng-Olive@users.noreply.github.com>
2026-04-06 18:08:10 +0100

    Update cloudinary.js

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,16 +1,13 @@
-// Import the Cloudinary library (v2 version) for handling cloud-based media storage
-import {v2 as cloudinary} from 'cloudinary'
+import { v2 as cloudinary } from "cloudinary";
 
-// Function to configure and connect Cloudinary
-const connectCloudinary = async ()=> {
-     // Set up Cloudinary configuration using environment variables
-    cloudinary.config({
-        cloud_name: process.env.CLOUDINARY_NAME,
-        api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_SECRET_KEY
-    })
+const connectCloudinary = async () => {
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+  });
 
-}
+  console.log("Cloudinary connected");
+};
 
-// Export the connectCloudinary function so it can be used in other parts of the project
-export default connectCloudinary
+export default connectCloudinary;
